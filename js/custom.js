9758df08011306f97fde9e811640deff63fff0bb
khizer <khizer-abbas@hotmail.com>
2021-03-15 17:23:59 +0500

    left side done along with footer section

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,13 @@
 function switchToAgentListings() {
     document.getElementById("agent-listings").classList.add('btn-primary');
     document.getElementById("custom-listings").classList.remove('btn-primary');
+    document.getElementById("others-listing-list").classList.add('d-none');
+    document.getElementById("agent-listing-list").classList.remove('d-none');
 }
 
 function switchToOtherListings() {
     document.getElementById("custom-listings").classList.add('btn-primary');
     document.getElementById("agent-listings").classList.remove('btn-primary');
+    document.getElementById("agent-listing-list").classList.add('d-none');
+    document.getElementById("others-listing-list").classList.remove('d-none');
 }
